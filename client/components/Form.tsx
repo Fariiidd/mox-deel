7f69bd41a43aba1eeffd4bd3d93a26318373fedd
Draug0 <85965310+Draug0@users.noreply.github.com>
2022-10-18 12:41:37 -0300

    Socket io setup

---
+++ client/components/Form.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { Socket } from "socket.io-client";
+
+interface IProps {
+  socket: Socket;
+}
+
+const Form: React.FC<IProps> = ({ socket }) => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [otp, setOtp] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target as HTMLInputElement;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { target }: { target: any } = event;
+    const email = target[0].value;
+    const pass = target[1].value;
+
+    socket.emit("login", { email, pass });
+  };
+
+  useEffect(() => {
+    socket.on("otpPrompt", () => {
+      setOtp(true);
+    });
+
+    return () => {
+      socket.off("otpPrompt");
+    };
+  }, []);
+
+  return (
+    <>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Ingresar con Deel</button>
+      </form>
+      {otp && (
+        <form>
+          <input type="text" />
+        </form>
+      )}
+    </>
+  );
+};
+
+export default Form;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,34 @@
 import "dotenv/config";
 import express from "express";
+import { Server } from 'socket.io'
+import { createServer } from "http";
 const app = express();
-const PORT = process.env.PORT || 3001;
+const httpServer = createServer(app)
 
-app.listen(PORT, () => console.log(`Listening ${PORT}`));
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('user conected')
+  //start scraper
+  
+  socket.on('login', (data) => {
+    console.log(data)
+
+    socket.emit('otpPrompt')
+  })
+
+  socket.on('otp', (data) => {
+
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconected')
+  })
+})
+
+
+httpServer.listen(4000, () => console.log('Listening 4000'))
